2191a557dab15de31aaff4be886f1fffca3e9daa
Major Tom <76742790+MajorTomDE@users.noreply.github.com>
2023-05-26 19:08:05 +0200

    Update motd-install.sh

--- motd-install.sh
+++ motd-install.sh
@@ -17,16 +17,20 @@ while true; do
             echo "Lade File A herunter..."
             curl -o "$target_directory/10-uname" -f https://raw.githubusercontent.com/MajorTomDE/motd-scripts/main/10-uname
             curl -o "$target_directory/20-sysinfo" -f https://raw.githubusercontent.com/MajorTomDE/motd-scripts/main/20-sysinfo
+            chmod +x "$target_directory/10-uname"
+            chmod +x "$target_directory/20-sysinfo"
             echo "Download von File A abgeschlossen."
             ;;
         2)
             echo "Lade File B herunter..."
             curl -o "$target_directory/30-docker" -f https://raw.githubusercontent.com/MajorTomDE/motd-scripts/main/30-docker
+            chmod +x "$target_directory/30-docker"
             echo "Download von File B abgeschlossen."
             ;;
         3)
             echo "Lade File C herunter..."
             curl -o "$target_directory/40-pihole" -f https://raw.githubusercontent.com/MajorTomDE/motd-scripts/main/40-docker
+            chmod +x "$target_directory/40-docker"
             echo "Download von File C abgeschlossen."
             ;;
         4)

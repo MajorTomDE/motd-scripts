6f7f13bc81e5a0751344d13badb98bd431cbec5d
Major Tom <76742790+MajorTomDE@users.noreply.github.com>
2023-05-26 10:51:26 +0200

    Update motd-install.sh

--- motd-install.sh
+++ motd-install.sh
@@ -2,4 +2,38 @@
 
 clear
 
-echo "This is a Test"
+while true; do
+    echo "Bitte wählen Sie eine Option:"
+    echo "1. File A herunterladen"
+    echo "2. File B herunterladen"
+    echo "3. File C herunterladen"
+    echo "4. Skript beenden"
+    read -p "Option auswählen (1-4): " option
+
+    case $option in
+        1)
+            echo "Lade File A herunter..."
+            wget -O file_a.txt <URL zum File A>
+            echo "Download von File A abgeschlossen."
+            ;;
+        2)
+            echo "Lade File B herunter..."
+            wget -O file_b.txt <URL zum File B>
+            echo "Download von File B abgeschlossen."
+            ;;
+        3)
+            echo "Lade File C herunter..."
+            wget -O file_c.txt <URL zum File C>
+            echo "Download von File C abgeschlossen."
+            ;;
+        4)
+            echo "Das Skript wird beendet."
+            exit 0
+            ;;
+        *)
+            echo "Ungültige Option ausgewählt."
+            ;;
+    esac
+
+    echo "------------------------------"
+done

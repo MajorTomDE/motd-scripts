0d68eeef5ee730d4d7eddaa8179cf61ae46174d2
Major Tom <76742790+MajorTomDE@users.noreply.github.com>
2025-09-29 00:29:34 +0200

    Update motd-install.sh

--- motd-install.sh
+++ motd-install.sh
@@ -10,9 +10,8 @@ while true; do
     echo "1. uname & sysinfo"
     echo "2. pihole"
     echo "3. docker"
-    echo "4. NordVPN"
     echo "8. Disable Last Logon Message"
-    echo "t. Test"
+    echo "t. Test motd"
     echo "e. Exit"
     echo ""
     read -p "Choose an option and press enter: " option
@@ -28,20 +27,14 @@ while true; do
             ;;
         2)
             echo "Lade File B herunter..."
-            curl -o "$target_directory/30-docker" -f https://raw.githubusercontent.com/MajorTomDE/motd-scripts/main/30-docker
-            chmod +x "$target_directory/30-docker"
+            curl -o "$target_directory/40-pihole" -f https://raw.githubusercontent.com/MajorTomDE/motd-scripts/main/40-pihole
+            chmod +x "$target_directory/40-pihole"
             echo "Download von File B abgeschlossen."
             ;;
         3)
             echo "Lade File C herunter..."
-            curl -o "$target_directory/40-pihole" -f https://raw.githubusercontent.com/MajorTomDE/motd-scripts/main/40-docker
-            chmod +x "$target_directory/40-docker"
-            echo "Download von File C abgeschlossen."
-            ;;
-        4)
-            echo "Lade File C herunter..."
-            curl -o "$target_directory/50-nordvpn" -f https://raw.githubusercontent.com/MajorTomDE/motd-scripts/main/50-nordvpn
-            chmod +x "$target_directory/50-nordvpn"
+            curl -o "$target_directory/30-docker" -f https://raw.githubusercontent.com/MajorTomDE/motd-scripts/main/30-docker
+            chmod +x "$target_directory/30-docker"
             echo "Download von File C abgeschlossen."
             ;;
         8)
